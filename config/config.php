ad942213a66cbc7813dba047120bec50da78e11f
Harry Houghton <h.oscar3@gmail.com>
2013-12-22 23:10:24 -0500

    fixing database listed in config file

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'h.oscar3@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'pr4_rubburduck_biz'); 
+	define('DB_NAME', 'rubburdu_pr4_rubburduck_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
